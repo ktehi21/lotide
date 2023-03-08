5b9ea1abdee4354a2c47d0c479e9d3517571d9e0
tehi_kim <k.tehi21@gmail.com>
2023-03-08 04:06:06 +0000

    console.assert result print with emoji

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  let trueResult = "✅✅✅ Assertion Passed: " + actual + " === " + expected;
+  let falseResult = "🛑🛑🛑 Assertion Failed: " + actual + "!== " + expected;
+  if (actual === expected) {
+    console.log(trueResult);
+  } else {
+    console.log(falseResult);
+  }
+  return;
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
