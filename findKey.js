39edeb82ad4337b5387e93de4df9d1b5bba19fa6
tehi_kim <k.tehi21@gmail.com>
2023-03-10 12:02:03 +0000

    find the each element key value from object array and check the asserEqual function

--- findKey.js
+++ findKey.js
@@ -10,23 +10,29 @@ const assertEqual = function(actual, expected) {
 };
 
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
 
-// 일단 arr를 다 뒤지는데 만약 genre랑 같은게 있으면 그거의 키를 알려줘봐
-const findKeyByValue = function(arr, genre) {
+
+// 일단 arr를 다 뒤지는데 만약 condition이랑 같은게 있으면 그거의 키를 알려줘봐
+const findKey = function(arr, condition) {
   for(const index in arr) {
-    // console.log("arr[index]:", arr[index]);
-    if(arr[index] === genre) {
-      console.log("index:", index, "/ arr[index]:", arr[index]);
-      return index
+    // console.log("index:", index, "/ arr[index]:", arr[index], 
+    // "/ arr[index].stars:", arr[index].stars);
+    // console.log("condition(arr[index]: ", condition(arr[index]));
+    if(condition(arr[index])) {
+      return index;
     }
   }
 };
 
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
 // TEST CODE
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
