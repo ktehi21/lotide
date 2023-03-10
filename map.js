2788420d6877e65c6665bfc1713b3bc8756e6d7e
tehi_kim <k.tehi21@gmail.com>
2023-03-10 12:35:50 +0000

    map can reach each elements of array and can change the values

---
+++ map.js
@@ -0,0 +1,38 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+
+  console.log('array: ', array);
+  console.log('callback: ', callback);
+
+  const results = [];
+  for (let item of array) {
+
+    const index = array.indexOf(item);
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback[index]);
+    console.log('---');
+  }
+  return results;
+}
+
+const results1 = words.map(word => word[0]);
+console.log(map(words,results1));
+
+// item BEFORE: ground
+// item AFTER: g
+// --
+// item BEFORE: control
+// item AFTER: c
+// --
+// item BEFORE: to
+// item AFTER: t
+// --
+// item BEFORE: major
+// item AFTER: m
+// --
+// item BEFORE: tom
+// item AFTER: t
+// --
+// [ ]
