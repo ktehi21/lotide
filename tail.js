cd1033b562addf03717c6dbb63c3e7282fdff81e
tehi_kim <k.tehi21@gmail.com>
2023-03-14 11:59:38 +0000

    import assertEqual

--- tail.js
+++ tail.js
@@ -1,17 +1,19 @@
-const assertEqual = function(actual, expected) {
-  let trueResult = `✅✅✅ Assertion Passed: ${actual}  === ${expected}`;
-  let falseResult = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  if (actual === expected) {
-    console.log(trueResult);
-  } else {
-    console.log(falseResult);
-  }
-  return;
-};
+// const assertEqual = function(actual, expected) {
+//   let trueResult = `✅✅✅ Assertion Passed: ${actual}  === ${expected}`;
+//   let falseResult = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+//   if (actual === expected) {
+//     console.log(trueResult);
+//   } else {
+//     console.log(falseResult);
+//   }
+//   return;
+// };
 
 // TEST CODE
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
+
+const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   if (arr.length === 0) {
@@ -24,6 +26,7 @@ const tail = function(arr) {
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 assertEqual(result.join(), ["Lighthouse", "Labs"].join());
+console.log(result.join(), ["Lighthouse", "Labs"].join());
 
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
