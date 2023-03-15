ee4aff5caec4759c7ab973e89196557c1bb5fff9
tehi_kim <k.tehi21@gmail.com>
2023-03-15 04:32:05 +0000

    Array.isArray is fine way to check array

---
+++ flatten.js
@@ -0,0 +1,43 @@
+
+const assertArraysEqual = function(actual, expected) {
+  let trueResult = `✅ Assertion Passed: ${actual}  == ${expected}`;
+  let falseResult = `🛑 Assertion Failed: ${actual} != ${expected}`;
+  if (actual == expected) {
+    console.log(trueResult);
+  } else {
+    console.log(falseResult);
+  }
+  return;
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const flatten = (arr) => {
+  let  result = [];
+  for (const x of arr) {
+    console.log("x: ", x);
+
+    if (Array.isArray(x) === true) {
+      for (const y of x) {
+        console.log("y: ", y);
+        result.push(y);
+      }
+    } else { result.push(x); }
+    
+  }
+  return console.log(result);
+};
+
+flatten([1, 2, [3, 4], 5, [6]])// => [1, 2, 3, 4, 5, 6]
