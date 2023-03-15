ebb8be8ba19126da8baea04004ff3c2c9359135f
tehi_kim <k.tehi21@gmail.com>
2023-03-15 09:18:17 +0000

    not just use return false --> result = false; return result;

--- eqArrays.js
+++ eqArrays.js
@@ -8,19 +8,27 @@
 //   }
 //   return;
 // };
+let j =0;
+let result = true;
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    // console.log("false in eqArray length");
+    result = false;
+    return result;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      // console.log(Array.isArray(arr1[i]), arr2[i])
+      if (Array.isArray(arr1[i]) === true && Array.isArray(arr2[i]) === true) { 
+        eqArrays(arr1[i], arr2[i]); 
+      } else if (arr1[i] !== arr2[i]) {
+        result = false;
+        return result;
+      }
     }
   }
-  return true;
+  return result;
 };
-
 
 // console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 // console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
@@ -29,6 +37,11 @@ const eqArrays = function(arr1, arr2) {
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);  // => should PASS
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
 
 
 module.exports = eqArrays;
