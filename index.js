36a23cf571b676951417a30ad29f1a80b05d84d2
tehi_kim <k.tehi21@gmail.com>
2023-03-15 10:05:47 +0000

    change the module.exports -shortening version

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 
+//// traditional way to write object key : value
+// module.exports = {
+//   head:   head,
+//   tail:   tail,
+//   middle: middle
+// };
+
+//// shortening 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle
 };
-
-
