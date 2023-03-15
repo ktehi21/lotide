7e9fad2af2ebb15a51a95caa4e46e14d0c5d7e51
tehi_kim <k.tehi21@gmail.com>
2023-03-15 07:40:06 +0000

    add eqArrays function and solve the problem that return true when first key values are different, if second key values are the samee

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,20 @@
+
+const eqArrays = function(arr1, arr2) {
+  // console.log("arr1.length: ", arr1.length);
+  if (arr1.length !== arr2.length) {
+    // console.log("length difference");
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      // console.log("arr1[i]", arr1[i]);
+      return false;
+    }
+  }
+  return true;
+};
+
+
 const assertEquals = function(actual, expected) {
   let trueResult = `✅ Assertion Passed: ${actual}  === ${expected}`;
   let falseResult = `🛑 Assertion Failed: ${actual} !== ${expected}`;
@@ -11,13 +28,20 @@ const assertEquals = function(actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-let result = [];
+let result = false;
 const eqObjects = function(object1, object2) {
     // console.log("Object.keys(object1)", Object.keys(object1).sort(), " /Object.keys(object2)",  Object.keys(object2).sort());
+    // console.log("eqArrays: ", eqArrays(Object.keys(object1), Object.keys(object2)));
+  // if (Object.keys(object1).sort().length != Object.keys(object2).sort().length) { result = false; console.log(result); return result}
+  
+  //// if keys are different return false
+  if (eqArrays(Object.keys(object1).sort(), Object.keys(object2).sort()) === false) { 
+    return false;
+  }
 
-  if (Object.keys(object1).sort().length != Object.keys(object2).sort().length) { result = false; console.log(result); return result}
+
+  // console.log("x: ",  "/ object2: ", object2.sort());
   for (const x in object1) {
-    // console.log("x: ", x, "/ object1.length: ", object1.length);
     for (const y in object2) {
       // console.log("y: ", y, "/ object2[y]: ", object2[y]);
       
@@ -25,11 +49,11 @@ const eqObjects = function(object1, object2) {
         if (object1[x] == object2[y]) {
 
           result = true;
-        }
-      } // else { result = false; }
+        } else { result = false; return result; }
+      } 
     }
   }
-  console.log(result);
+  // console.log(result);
   return result;
 };
 
